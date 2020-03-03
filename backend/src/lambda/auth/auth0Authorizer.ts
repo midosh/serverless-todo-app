8901707119033a309e72f227db15046a8ffd0891
ahmed <eng.mido.sharaf1@gmail.com>
2020-03-03 23:58:47 +0200

    final project

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -14,7 +14,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://sls-udagram.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://sls-todo.auth0.com/.well-known/jwks.json'
 const jwkToPem = require('jwk-to-pem');
 
 export const handler = async (
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '6qc6dzm20h'
+const apiId = 'r0eu2x4kq1'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'sls-udagram.auth0.com',            // Auth0 domain
-  clientId: 'KbqVZ1cvKiKQCTWED9grzpE9z4H30rS9',          // Auth0 client id
+  domain: 'sls-todo.auth0.com',            // Auth0 domain
+  clientId: 'LpUVMu0AA2v0gOE4dCo9FCL3F9NwSQE3',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
